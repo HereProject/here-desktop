e0ee5d5b0cb0c3a0f27125eff1ded510db33cb56
mertkaant <36658227+mertkaant@users.noreply.github.com>
2019-11-11 19:27:16 +0300

    Proje kodları

---
+++ Here/Here/Formlar/Menu.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Here.Formlar
+{
+    partial class Menu
+    {
+        /// <summary> 
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Bileşen Tasarımcısı üretimi kod
+
+        /// <summary> 
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_yoklama = new System.Windows.Forms.Button();
+            this.btn_dersler = new System.Windows.Forms.Button();
+            this.btn_cikis = new System.Windows.Forms.Button();
+            this.btn_kullanici = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_yoklama
+            // 
+            this.btn_yoklama.BackColor = System.Drawing.Color.Transparent;
+            this.btn_yoklama.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_yoklama.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_yoklama.ForeColor = System.Drawing.Color.RosyBrown;
+            this.btn_yoklama.Location = new System.Drawing.Point(35, 287);
+            this.btn_yoklama.Name = "btn_yoklama";
+            this.btn_yoklama.Size = new System.Drawing.Size(188, 71);
+            this.btn_yoklama.TabIndex = 1;
+            this.btn_yoklama.Text = "Yoklama İşlemleri";
+            this.btn_yoklama.UseVisualStyleBackColor = false;
+            this.btn_yoklama.Click += new System.EventHandler(this.btn_yoklama_Click);
+            // 
+            // btn_dersler
+            // 
+            this.btn_dersler.BackColor = System.Drawing.Color.Transparent;
+            this.btn_dersler.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_dersler.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dersler.ForeColor = System.Drawing.Color.RosyBrown;
+            this.btn_dersler.Location = new System.Drawing.Point(35, 378);
+            this.btn_dersler.Name = "btn_dersler";
+            this.btn_dersler.Size = new System.Drawing.Size(188, 71);
+            this.btn_dersler.TabIndex = 2;
+            this.btn_dersler.Text = "Ders İşlemleri";
+            this.btn_dersler.UseVisualStyleBackColor = false;
+            this.btn_dersler.Click += new System.EventHandler(this.btn_dersler_Click);
+            // 
+            // btn_cikis
+            // 
+            this.btn_cikis.BackColor = System.Drawing.Color.Transparent;
+            this.btn_cikis.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cikis.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cikis.ForeColor = System.Drawing.Color.RosyBrown;
+            this.btn_cikis.Location = new System.Drawing.Point(35, 555);
+            this.btn_cikis.Name = "btn_cikis";
+            this.btn_cikis.Size = new System.Drawing.Size(188, 71);
+            this.btn_cikis.TabIndex = 3;
+            this.btn_cikis.Text = "Çıkış";
+            this.btn_cikis.UseVisualStyleBackColor = false;
+            this.btn_cikis.Click += new System.EventHandler(this.btn_cikis_Click);
+            // 
+            // btn_kullanici
+            // 
+            this.btn_kullanici.BackColor = System.Drawing.Color.Transparent;
+            this.btn_kullanici.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_kullanici.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_kullanici.ForeColor = System.Drawing.Color.RosyBrown;
+            this.btn_kullanici.Location = new System.Drawing.Point(35, 467);
+            this.btn_kullanici.Name = "btn_kullanici";
+            this.btn_kullanici.Size = new System.Drawing.Size(188, 71);
+            this.btn_kullanici.TabIndex = 4;
+            this.btn_kullanici.Text = "Kullanıcı İşlemleri";
+            this.btn_kullanici.UseVisualStyleBackColor = false;
+            this.btn_kullanici.Click += new System.EventHandler(this.btn_kullanici_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Here.Properties.Resources.Fu2;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(229, 185);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.Controls.Add(this.btn_kullanici);
+            this.Controls.Add(this.btn_cikis);
+            this.Controls.Add(this.btn_dersler);
+            this.Controls.Add(this.btn_yoklama);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Menu";
+            this.Size = new System.Drawing.Size(253, 687);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_yoklama;
+        private System.Windows.Forms.Button btn_dersler;
+        private System.Windows.Forms.Button btn_cikis;
+        private System.Windows.Forms.Button btn_kullanici;
+    }
+}
---
+++ Here/Here/GirisForm.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Here.Formlar
+{
+    partial class GirisForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ustPanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuPanel = new System.Windows.Forms.Panel();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.forgotPass = new System.Windows.Forms.Label();
+            this.btn_giris = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_sifre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ustPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.mainPanel.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ustPanel
+            // 
+            this.ustPanel.BackColor = System.Drawing.Color.RosyBrown;
+            this.ustPanel.Controls.Add(this.pictureBox1);
+            this.ustPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ustPanel.Location = new System.Drawing.Point(0, 0);
+            this.ustPanel.Name = "ustPanel";
+            this.ustPanel.Size = new System.Drawing.Size(1300, 57);
+            this.ustPanel.TabIndex = 0;
+            this.ustPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ustPanel_MouseDown);
+            this.ustPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ustPanel_MouseMove);
+            this.ustPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ustPanel_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Here.Properties.Resources.x;
+            this.pictureBox1.Location = new System.Drawing.Point(1253, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // menuPanel
+            // 
+            this.menuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuPanel.Location = new System.Drawing.Point(0, 57);
+            this.menuPanel.Name = "menuPanel";
+            this.menuPanel.Size = new System.Drawing.Size(253, 687);
+            this.menuPanel.TabIndex = 8;
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.groupBox1);
+            this.mainPanel.Controls.Add(this.label1);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(253, 57);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(1047, 687);
+            this.mainPanel.TabIndex = 9;
+            this.mainPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.mainPanel_Paint);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.forgotPass);
+            this.groupBox1.Controls.Add(this.btn_giris);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.txt_id);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txt_sifre);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(154, 102);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(503, 556);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // forgotPass
+            // 
+            this.forgotPass.AutoSize = true;
+            this.forgotPass.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.forgotPass.ForeColor = System.Drawing.Color.RosyBrown;
+            this.forgotPass.Location = new System.Drawing.Point(274, 415);
+            this.forgotPass.Name = "forgotPass";
+            this.forgotPass.Size = new System.Drawing.Size(186, 23);
+            this.forgotPass.TabIndex = 8;
+            this.forgotPass.Text = "Şifrenizi mi unuttunuz?";
+            // 
+            // btn_giris
+            // 
+            this.btn_giris.BackColor = System.Drawing.Color.Transparent;
+            this.btn_giris.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_giris.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_giris.ForeColor = System.Drawing.Color.RosyBrown;
+            this.btn_giris.Location = new System.Drawing.Point(156, 464);
+            this.btn_giris.Name = "btn_giris";
+            this.btn_giris.Size = new System.Drawing.Size(217, 57);
+            this.btn_giris.TabIndex = 8;
+            this.btn_giris.Text = "Giriş";
+            this.btn_giris.UseVisualStyleBackColor = false;
+            this.btn_giris.Click += new System.EventHandler(this.btn_giris_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Here.Properties.Resources.Fu2;
+            this.pictureBox2.Location = new System.Drawing.Point(119, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(285, 227);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // txt_id
+            // 
+            this.txt_id.BackColor = System.Drawing.Color.Linen;
+            this.txt_id.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_id.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_id.ForeColor = System.Drawing.Color.RosyBrown;
+            this.txt_id.Location = new System.Drawing.Point(168, 308);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(285, 43);
+            this.txt_id.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.RosyBrown;
+            this.label3.Location = new System.Drawing.Point(62, 369);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 38);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Şifre:";
+            // 
+            // txt_sifre
+            // 
+            this.txt_sifre.BackColor = System.Drawing.Color.Linen;
+            this.txt_sifre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_sifre.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_sifre.ForeColor = System.Drawing.Color.RosyBrown;
+            this.txt_sifre.Location = new System.Drawing.Point(168, 369);
+            this.txt_sifre.Name = "txt_sifre";
+            this.txt_sifre.Size = new System.Drawing.Size(285, 43);
+            this.txt_sifre.TabIndex = 3;
+            this.txt_sifre.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.RosyBrown;
+            this.label2.Location = new System.Drawing.Point(62, 308);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 38);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "E-mail:";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.RosyBrown;
+            this.label1.Location = new System.Drawing.Point(223, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(423, 64);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Here!\'a Hoşgeldiniz..";
+            // 
+            // GirisForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.ClientSize = new System.Drawing.Size(1300, 744);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.menuPanel);
+            this.Controls.Add(this.ustPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "GirisForm";
+            this.Text = "GirisForm";
+            this.ustPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.mainPanel.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ustPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.Panel mainPanel;
+        public System.Windows.Forms.Panel menuPanel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label forgotPass;
+        private System.Windows.Forms.Button btn_giris;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_sifre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
